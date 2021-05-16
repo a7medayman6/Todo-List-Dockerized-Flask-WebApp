f2a6309ea808de43ab6d6699041e08d3ab1076b6
AHMED AYMAN <a.ayman6000@gmail.com>
2021-05-16 14:21:59 +0200

    SPECIFIED THE PORT IN DOCKERFILE

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/db.sqlite'
